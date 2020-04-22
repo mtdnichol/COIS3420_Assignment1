a80ef918bfebbab87867e3124af1ed1757120448
Jared <jmezzbmxer@live.ca>
2020-04-21 20:48:48 -0400

    Modified header partial

--- DisplayList.php
+++ DisplayList.php
@@ -28,18 +28,8 @@ $results = $statement->fetchAll();
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Bucket List</title>
-    <link rel="stylesheet" href="css/MainStyle.css">
-    <link href="https://fonts.googleapis.com/css?family=Fredoka+One|Lato:300,400,700|Roboto:300,400,700&display=swap" rel="stylesheet">
-    <script defer src="./scripts/logout.js"></script>
-    <script src="https://kit.fontawesome.com/1c8ee6a0f5.js" crossorigin="anonymous"></script>
-</head>
-<body>
-    <?php include "./includes/header.php"; ?>
+<!--html starts-->
+<?php include "./includes/header.php"; ?>
     <div class="main-box">
         <h1><?php echo $_SESSION['username']?>'s Bucket List</h1>
         <h2><?php echo $title['title'] ?></h2>
