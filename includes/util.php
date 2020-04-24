a5d94816fb8a985e1467be2308f44ecfddcf7b67
Dctr <hiimdctr@gmail.com>
2020-04-24 00:29:46 -0400

    use globals

--- includes/util.php
+++ includes/util.php
@@ -1,19 +1,18 @@
 <?php
+$pdo = connectDB();
 
 function isOwner($id) {
-    $pdo = connectDB();
     $username = $_SESSION['username'];
     $query = "SELECT bucket_users.username FROM bucket_lists INNER JOIN bucket_users ON bucket_lists.fk_userid = bucket_users.id WHERE bucket_lists.id = ?";
-    $statement = $pdo->prepare($query);
+    $statement = $GLOBALS['pdo']->prepare($query);
     $statement->execute([$id]);
     $results = $statement->fetch();
     return !empty($results) && $results['username'] == $username;
 }
 
 function isPrivate($id) {
-    $pdo = connectDB();
     $query = "SELECT private FROM bucket_lists WHERE id = ?";
-    $statement = $pdo->prepare($query);
+    $statement = $GLOBALS['pdo']->prepare($query);
     $statement->execute([$id]);
     $results = $statement->fetch();
     return !empty($results) && $results['private'] === 1;
