8149f22be850f7808e7f5e3ce32c96d95e3adff1
MichaelDesktop <mtdnichol@gmail.com>
2020-04-23 22:05:58 -0400

    up to speed

--- Register.php
+++ Register.php
@@ -65,11 +65,9 @@ if (isset($_POST['submit'])) {
     <meta charset="UTF-8">
     <title>Register</title>
     <link rel="stylesheet" href="css/MainStyle.css">
+    <link rel="stylesheet" href="./plugins/passwordStrength/pwdStyles.css">
     <link href="https://fonts.googleapis.com/css?family=Fredoka+One|Lato:300,400,700|Roboto:300,400,700&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/1c8ee6a0f5.js" crossorigin="anonymous"></script>
-    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-    <script type="text/javascript" src="strength.js"></script>
-    <script type="text/javascript" src="js.js"></script>
 </head>
 <body>
     <div class="main-box">
