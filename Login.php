02dcd54b19138ec4308a40de4121dea3d8d33f80
MichaelDesktop <mtdnichol@gmail.com>
2020-04-23 23:47:29 -0400

    finished recover password

--- Login.php
+++ Login.php
@@ -27,17 +27,26 @@ if (isset($_POST['submit'])) {
     /* Check the database for occurrences of $username */
     $query = "SELECT id, username, password FROM `bucket_users` WHERE username = ?";
     $statement = $pdo->prepare($query);
-
     $statement->execute([ $username ]);
     $results = $statement->fetch();
-
-
 
     if ($results === FALSE) {
         array_push($errors, "That user doesn't exist.");
     } else if (password_verify($password, $results['password'])) {
         $_SESSION['username'] = $username;
         $_SESSION['userID'] = $results['id'];
+
+        //Check if the remember me function has been selected  ->https://tutorialsclass.com/code/php-login-remember-cookies-script
+        if(!empty($_POST["remember"])) {
+            setcookie ("username",$_POST["username"],time()+ 3600);
+            setcookie ("password",$_POST["password"],time()+ 3600);
+            echo "Cookies Set Successfuly";
+        } else {
+            setcookie("username","");
+            setcookie("password","");
+            echo "Cookies Not Set";
+        }
+
         header("Location: Profile");
         exit();
     } else {
@@ -75,11 +84,15 @@ if (isset($_POST['deleteList'])){
             <form id="main-form" action="<?= $_SERVER['PHP_SELF'] ?>" method="POST"> <!-- Redirect to DisplayList.html upon completion -->
                 <div>
                     <label for="username"><i class="fas fa-envelope"></i></label>
-                    <input id="username" name="username" type="text" placeholder="Username">
+                    <input id="username" name="username" type="text" placeholder="Username" value="<?php if(isset($_COOKIE["username"])) { echo $_COOKIE["username"]; } ?>">
                 </div>
                 <div>
                     <label for="password"><i class="fas fa-lock"></i></label>
-                    <input id="password" name="password" type="text" placeholder="Password">
+                    <input id="password" name="password" type="password" placeholder="Password" value="<?php if(isset($_COOKIE["password"])) { echo $_COOKIE["password"]; } ?>">
+                </div>
+                <div>
+                    <input type="checkbox" id="remember">
+                    <label for="remember">Remember me</label>
                 </div>
 
                 <button id="submit" name="submit" class="centered">Login</button>
@@ -88,8 +101,8 @@ if (isset($_POST['deleteList'])){
                 <?php endforeach; ?>
             </form>
         </div>
-        <p id="reset" class="hidden">Forgot your password?  <a href="Register" class="inline">Reset</a></p>
         <p>Don't have an account?  <a href="Register" class="inline">Register</a> instead.</p>
+        <p>Forgot your password?  <a href="RecoverPassword" class="inline">Click here!</a></p>
     </div>
 </body>
 </html>
