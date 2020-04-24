f0e19071c5a03bf1081cf590251bd044b2e52293
MichaelDesktop <mtdnichol@gmail.com>
2020-04-24 06:00:43 -0400

    wd

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
 
-<script src="https://unpkg.com/popper.js@1"></script>
+<script src="https://unpkg.com/popper.js@1"></script> <!-- On hover javascript text for list and item button actions -->
 <script src="https://unpkg.com/tippy.js@5"></script>
 <script>
     tippy('[data-tippy-content]');
